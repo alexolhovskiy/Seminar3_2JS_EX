ff055590c6c9a897914679f1ec601aed32a6553f
Sasha Sasha <sashasashaolh@gmail.com>
2024-04-08 21:43:10 +0300

    Seminar6

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
-//alert("Hello");
-//let first = prompt("Enter your name");
-//if (confirm("is this your real name?")) {
-//    alert("OK! Yuor name is "+first);
-//} else {
-//    alert("NOT OK!");
-//}
-
 //1
-let num1 = prompt("Enter num1");
-let num2 = prompt("Enter num2");
-
-alert(`${num1<=1?"Yes num1<=1":"No num1>1"}`);
-alert(`${num2 >= 3 ? "Yes num2>=3" : "No num2<3"}`);
-
-//2
-let test = true;
-//if (test === true) {
-//    console.log('+++');
-//} else {
-//    console.log('---');
-//}
-
-test ? console.log('+++') : console.log('---');
-
-//3
-let day = Math.floor( 1 + Math.random() * 30);
-console.log("Our number is "+day);
-if (day > 1 && day < 11) {
-    console.log("First decade");
-} else {
-    if (day > 10 && day < 21) {
-        console.log("Second decade");
-    } else {
-        console.log("Third decade");
-    }
+function getQub(num) {
+    return num * num * num;
 }
 
+console.log(getQub(2) + "+" + getQub(3));
+
+//2
+let salary = prompt("Enter salary");
+//if (isNaN(parseInt(salary))) {
+//    console.log("Entrance not correct!");
+//} else {
+//    salary -= salary * 0.13;
+//    console.log(`The amount of wages minus taxes is equal to the value ${salary}`);
+//}
+
+
+let regexp = /\d*[^\d]/;
+
+if (regexp.test(salary)) {
+    console.log("Entrance not correct!");
+} else {
+    salary -= salary * 0.13;
+    console.log(`The amount of wages minus taxes is equal to the value ${salary}`);
+}
+
+//3
+let arr = prompt("Enter three numbers(like 1 2 3)").split(" ");
+arr.forEach((item) => console.log(item));
+let arrMax = (arr) => {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[max]) {
+            max = i;
+        }
+    }
+    return arr[max];
+};
+console.log("Max value is " + arrMax(arr));
+
+
 //4
-let num = prompt("Enter number");
-let hundreds = Math.floor(num / 100) % 10;
-let tens = Math.floor(num / 10) % 10;
-let units = num % 10;
-console.log(`In the number ${num} there are hundreds: ${hundreds}, tens: ${tens}, units: ${units}`);
+
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+function getDifference(num1, num2) {
+    return num1 >= num2 ? num1 - num2 : num2 - num1;
+}
+
+function getMult(num1, num2) {
+    return num1 * num2;
+}
+
+function getQuotient(num1,num2) {
+    return num1 / num2;
+}
+
+console.log(`Sum ${getSum(2, 6)}`);
+console.log(`Difference ${getDifference(2, 6)}`);
+console.log(`Mult ${getMult(2, 6)}`);
+console.log(`Division ${getQuotient(2, 6)}`);
+
+
+
+
+
+
+
+
+
+
+
