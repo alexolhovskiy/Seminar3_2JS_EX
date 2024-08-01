82bc412cbbb0058104fda3cbc95ca8012b8c35fa
Sasha Sasha <sashasashaolh@gmail.com>
2024-08-01 11:39:46 +0300

    Seminar2

---
+++ data_file.js
@@ -0,0 +1,1 @@
+const data=`[{"name": "bander_pay_life.jpg"}, {"name": "bender-bending-rodriguez.jpg"}, {"name": "bender_anger.jpg"}, {"name": "bender_bad_feeling.jpg"}, {"name": "bender_carts.jpg"}, {"name": "bender_cookies.jpg"}, {"name": "bender_fire.jpg"}, {"name": "bender_get_up_ze_way.jpg"}, {"name": "bender_poster.jpg"}, {"name": "bender_smooke.jpg"}]`;
--- script.js
+++ script.js
@@ -1,12 +1,104 @@
-//1
+console.log("Hi!")
 
-//const buttonEl = document.querySelector('.button');
-//const productImg = document.querySelector('.product__img');
-////productImg.onclick = function () {
-////    productImg.src = 'robot.jpg';
-////    productImg.alt = "new img";
-////}
-//buttonEl.onclick = function () {
-//    buttonEl.textContent = 'Товар добавлен в корзину';
-//    productImg.src = 'robot.jpg';
-//}
+let arr=JSON.parse(data);
+console.log(arr);
+
+let num=arr.length,frame=5;
+if(num<frame){
+    frame=num;
+}
+let img=document.querySelector(".img");
+img.src = 'img2/'+arr[0].name;
+let slider=document.querySelector(".slider");
+var id=0,index=0;
+
+function addPoints(){
+    slider.insertAdjacentHTML('beforeend',`<img id="5" class="pointer" src="__2208.png"></img>`);
+    slider.insertAdjacentHTML('beforeend',`<div id="${0}" class="pointer selection">${1}</div>`);
+    for (let i = 1; i < frame; i++) {
+        slider.insertAdjacentHTML('beforeend',`<div id="${i}" class="pointer">${i+1}</div>`);
+    }
+    slider.insertAdjacentHTML('beforeend',`<img id="6" class="pointer" src="__2207.png"></img>`);
+}
+
+addPoints();
+
+function sliderShift(shift){
+    for(var i=0;i<slider.children.length;i++){
+        if(Number(slider.children[i].getAttribute("id"))<5){
+            slider.children[i].textContent=+(Number(slider.children[i].textContent)+shift);
+        }
+    }
+}
+
+function idShift(oldId,newId){
+    document.querySelector("#"+CSS.escape(oldId)).classList.toggle("selection");
+    document.querySelector("#"+CSS.escape(newId)).classList.toggle("selection");
+    id=newId;
+}
+
+slider.addEventListener("click",(e)=>{
+    switch(e.target.getAttribute("id")){
+        
+        case "0":idShift(id,0);break;
+        case "1":idShift(id,1);break;
+        case "2":idShift(id,2);break;
+        case "3":idShift(id,3);break;
+        case "4":idShift(id,4);break;
+        case "5":
+            if(num>frame){
+                if(id>2){
+                    idShift(id,id-1);
+                }else{
+                    if(index>0){
+                        index--;
+                        sliderShift(-1);
+                    }else{
+                        if(id>0){
+                            idShift(id,id-1);
+                        }else{
+                            sliderShift(num-frame);
+                            index=num-frame;
+                            idShift(id,(frame-1));
+                        }
+                    }
+                }
+            }else{
+                if(id>0){
+                    idShift(id,id-1);
+                }else{
+                    idShift(id,(frame-1));
+                }
+            }
+            break;
+
+        case "6":
+            if(num>frame){
+                if(id<2){
+                    idShift(id,id+1);
+                }else{
+                    if(index<(num-frame)){
+                        index++;
+                        sliderShift(1);
+                    }else{
+                        if(id<(frame-1)){
+                            idShift(id,id+1);
+                        }else{
+                            sliderShift(-index);
+                            index=0;
+                            idShift(id,0);
+                        }
+                    }
+                }
+            }else{
+                if(id<(frame-1)){
+                    idShift(id,id+1);
+                }else{
+                    idShift(id,0);
+                }
+            }
+            break;
+    }
+    //console.log(id+" "+index);
+    img.src = 'img2/'+arr[index+id].name;
+});
